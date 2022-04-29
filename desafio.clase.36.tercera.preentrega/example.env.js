b284b5aac08d0524f44378f3fff8f1db4a1e5ed1
Alan Gimenez <gimenezalan00@gmail.com>
2022-04-28 23:31:41 -0300

    archivo example .env

--- desafio.clase.36.tercera.preentrega/example.env.js
+++ desafio.clase.36.tercera.preentrega/example.env.js
@@ -2,7 +2,7 @@ PORT=8080
 # la persistencia solo puede ser en mongodb
 PERSISTENCIA="mongodb"
 # se coloca la ruta uri de mongodb
-MONGODB_URI=`mongodb+srv://leonel654321:C0mput4d0r4s.@coderhouse.7epn5.mongodb.net/mySecondDatabase?retryWrites=true&w=majority`
+MONGODB_URI=`uri mongo`
 # se coloca la ruta del archivo .json para acceder a firebase
 FIREBASE_ROUTE='../../databases/backend-ecommerce-c1032-firebase-adminsdk-n0gj7-82ea707c9c.json'
 
@@ -10,10 +10,10 @@ FIREBASE_ROUTE='../../databases/backend-ecommerce-c1032-firebase-adminsdk-n0gj7-
 NODE_TLS_REJECT_UNAUTHORIZED='0' 
 
 # datos para acceso a twilio
-ACCOUNT_TWILIO='AC125d0467ea87fc4ce2d76c7c5052050a'
-TOKEN_TWILIO='e77390bc41128418ad36959a4c9b2a51'
+ACCOUNT_TWILIO='account twilio'
+TOKEN_TWILIO='toker twilio'
 NUMBER_SMS_TWILIO='+18597150942'
 
 # datos para acceso nodemailer
-USER_NODEMAILER='larry.heaney3@ethereal.email'
-PASS_NODEMAILER='jSa9ugqdTtJAa6PUvr'
+USER_NODEMAILER='mail nodemailer'
+PASS_NODEMAILER='password nodemailer'
